7c6f048485bc5d856585c822b116b88c8599ab17
Sweetyamnesia <angela.r@live.fr>
2025-05-05 14:04:48 +0200

    update string and replace with new string

---
+++ python-hello_world/8-concat_edges.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+
+str1 = "object-oriented programming with Python"
+
+str = str.replace(str, str1)
+print(str)
