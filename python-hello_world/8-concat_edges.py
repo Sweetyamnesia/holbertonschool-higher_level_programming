1d1baf1cbb44b776ed60a4f3b8981f9d8836dcff
Sweetyamnesia <angela.r@live.fr>
2025-05-05 19:15:25 +0200

    delete one variable and change string inside str replace

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-str1 = "object-oriented programming with Python"
-str = str.replace(str, str1)
+str = str.replace("object-oriented programming", "object-oriented programming with Python")
 print(str)
