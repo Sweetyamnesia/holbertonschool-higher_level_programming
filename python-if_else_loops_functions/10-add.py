85326e040c3723934952dda2917585d3d0ec3ae2
Sweetyamnesia <angela.r@live.fr>
2025-05-06 20:28:29 +0200

    pycodestyle

--- python-if_else_loops_functions/10-add.py
+++ python-if_else_loops_functions/10-add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def add(a, b):
-	sum = int(a) + int(b)
-	return(sum)
+    sum = int(a) + int(b)
+    return (sum)
--- python-if_else_loops_functions/11-pow.py
+++ python-if_else_loops_functions/11-pow.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def pow(a, b):
-	if b < 0:
-		return(-1)
-	elif b == 0:
-		return(1)
-	return(a * pow(a, b -1))
+    if b < 0:
+        return (-1)
+    elif b == 0:
+        return (1)
+    return (a * pow(a, b - 1))
