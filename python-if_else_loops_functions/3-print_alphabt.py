a7a1ec118838136455efc8c6d2d9445458bf5f2f
Sweetyamnesia <angela.r@live.fr>
2025-05-06 14:42:02 +0200

    add ord

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(97, 123):
+for alphabet in range(ord('a'), ord('z') + 1):
     if chr(alphabet) not in ('q', 'e'):
         print(f"{chr(alphabet)}", end="")
