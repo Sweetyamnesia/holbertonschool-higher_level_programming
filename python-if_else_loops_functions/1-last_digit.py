689800c535393b8fef27f1f85fd004ba207336ed
Sweetyamnesia <angela.r@live.fr>
2025-05-06 12:22:41 +0200

    add %10 inside brackets

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,9 +2,9 @@
 
 import random
 number = random.randint(-10000, 10000)
-if number > 5:
-    print(f"Last digit of {number} is {number} and is greater than 5")
-if number == 0:
-    print(f"Last digit of {number} is {number} and is 0 ")
-if number < 6 and number != 0:
-    print(f"Lat digit of {number} is {number} and is less than 6 and not 0")
+if (number % 10 > 5):
+    print(f"Last digit of {number} is {number % 10} and is greater than 5")
+if (number % 10 == 0):
+    print(f"Last digit of {number} is {number % 10} and is 0", number)
+if (number % 10 < 6 and number % 10 != 0):
+    print(f"Lat digit of {number} is {number % 10} and is less than 6 and not 0")
