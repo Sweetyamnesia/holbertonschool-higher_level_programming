3086f60a29d8abf78bc8288f31b9939f29a4575a
Sweetyamnesia <angela.r@live.fr>
2025-05-06 19:09:25 +0200

    fix : Replaced f-strings with format()

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for n in range(0, 9):
     for j in range(n + 1, 10):
         if n == 8 and j == 9:
-            print(f"{n}{j}")
+            print("{}{}".format(n, j))
         else:
-            print(f"{n}{j}, ", end="")
+            print("{}{}, ".format(n, j), end="")
