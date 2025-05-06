14127c9d5bda2d6ccb739465512053d46ed85115
Sweetyamnesia <angela.r@live.fr>
2025-05-06 15:05:23 +0200

    use range instead of ord and use format

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for alphabet in range(ord('a'), ord('z') + 1):
-    if chr(alphabet) not in ('q', 'e'):
-        print(f"{chr(alphabet)}", end="")
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{}".format(chr(i)), end="")
