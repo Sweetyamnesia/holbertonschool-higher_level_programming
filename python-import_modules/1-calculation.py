44995600d8e934cc41710309b810c2b931a717fb
Sweetyamnesia <angela.r@live.fr>
2025-05-07 10:54:59 +0200

    add calculation before and the bracket

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{}".format(add(a, b)))
-    print("{}".format(sub(a, b)))
-    print("{}".format(mul(a, b)))
-    print("{}".format(div(a, b)))
+    print("10 + 5 = {}".format(add(a, b)))
+    print("10 - 5 = {}".format(sub(a, b)))
+    print("10 * 5 = {}".format(mul(a, b)))
+    print("10 / 5 = {}".format(div(a, b)))
