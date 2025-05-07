390fc657cf997a8428865eb57a5dc4d09bc3fb51
Sweetyamnesia <angela.r@live.fr>
2025-05-07 11:27:00 +0200

    add file

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-
+if __name__ == "__main__":
+    def args(argc, argv):
+        if argc == 1:
+            print("O arguments.")
+        elif argc == 2:
+            print("1 argument:")
+        else:
+            print("{} arguments".format(argc - 1))
+            
+        for n in range(1, argc):
+            print(f"{n}: {argv[n]}")
