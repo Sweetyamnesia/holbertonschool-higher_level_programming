67482204ef6e3a145ece22633a1c3c08853e19bd
Sweetyamnesia <angela.r@live.fr>
2025-05-07 09:19:38 +0200

    pycodestyle

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def uppercase(str):
-	if ord(str) in range(97, 123):
-		if str == uppercase:
-			print("{}".format(str))
-	else:
-		print("{}".format(str), end="")
+    if ord(str) in range(97, 123):
+        if str == uppercase:
+            print("{}".format(str))
+    else:
+        print("{}".format(str), end="")
