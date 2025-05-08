e3b5c0579302eaf781a7de45aec13d2eb49e2659
Sweetyamnesia <angela.r@live.fr>
2025-05-08 07:22:57 +0200

    pycodestyle

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 import sys
 if __name__ == "__main__":
-	n = len(sys.argv)
-	if len(sys.argv) == 1:
-		print("{} argument.".format(n - 1), end="\n")
-	elif len(sys.argv) == 2:
-		print("{} argument:".format(n - 1), end="\n")
-	else:
-		print("{} arguments:".format(n - 1), end="\n")
+    n = len(sys.argv)
+    if len(sys.argv) == 1:
+        print("{} argument.".format(n - 1), end="\n")
+    elif len(sys.argv) == 2:
+        print("{} argument:".format(n - 1), end="\n")
+    else:
+        print("{} arguments:".format(n - 1), end="\n")
 
-	for i in range(1, n):
-		print("{}: {}".format(i, sys.argv[i]), end="\n")
+    for i in range(1, n):
+        print("{}: {}".format(i, sys.argv[i]), end="\n")
