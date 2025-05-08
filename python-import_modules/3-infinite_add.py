73a81ddc22f828aa6f429f1531d8e17cee9fa82e
Sweetyamnesia <angela.r@live.fr>
2025-05-08 07:47:11 +0200

    Fix: replace incorrect argument loop with sum of int(argv[1:]) and print result

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -2,6 +2,6 @@
 import sys
 if __name__ == "__main__":
     sum = 0
-    args = sys.argv[1:]
-    for args in sys.argv:
-        print("{}".format(sys.argv))
+    for n in range(1, len(sys.argv)):
+        sum += int(sys.argv[n])
+    print(sum)
