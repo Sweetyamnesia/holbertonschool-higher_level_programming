a8447451b0584f05615cc17411a9f64d3baeed6e
Sweetyamnesia <angela.r@live.fr>
2025-05-08 07:21:19 +0200

    Fix argument count display and print each argument with its position

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -3,8 +3,11 @@ import sys
 if __name__ == "__main__":
 	n = len(sys.argv)
 	if len(sys.argv) == 1:
-		print("{} arguments.".format(sys.argv[0]))
+		print("{} argument.".format(n - 1), end="\n")
 	elif len(sys.argv) == 2:
-		print("{} argument:".format(sys.argv[1]), end="\n")
+		print("{} argument:".format(n - 1), end="\n")
 	else:
-		print("{} arguments:".format(sys.argv), end="\n")
+		print("{} arguments:".format(n - 1), end="\n")
+
+	for i in range(1, n):
+		print("{}: {}".format(i, sys.argv[i]), end="\n")
