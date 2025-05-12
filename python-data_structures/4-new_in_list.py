d514b5b3697e492c477f825eeb29a7ec770fab52
Sweetyamnesia <angela.r@live.fr>
2025-05-12 15:01:03 +0200

    fix(new_in_list): replaced incorrect loop with proper index check and element modification

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def new_in_list(my_list, idx, element):
-	for element in my_list:
-		if idx < element:
-			return my_list.copy
-		elif idx > element:
-			return my_list.copy
-
+    if 0 <= idx < len(my_list):
+        if idx < element:
+            new_list = my_list.copy()
+            new_list[idx] = element
+            return new_list
+        return my_list.copy()
