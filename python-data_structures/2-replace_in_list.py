6ab7a9c722060197fd1902d14be0dde22cf35fd1
Sweetyamnesia <angela.r@live.fr>
2025-05-12 15:47:11 +0200

    fix(replace_in_list): replace element at specified index and return the full list

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -2,7 +2,7 @@
 
 def replace_in_list(my_list, idx, element):
     if 0 <= idx < len(my_list):
-        if idx < element:
-            return my_list[idx]
-        else:
-            return my_list
+        my_list[idx] = element
+        return my_list
+    else:
+        return my_list
