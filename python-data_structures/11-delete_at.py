0b58601a824bdbfc3ea8db7df4553bef82cb3123
Sweetyamnesia <angela.r@live.fr>
2025-05-12 17:12:17 +0200

    Fix: Validate index and delete element from list

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
 def delete_at(my_list=[], idx=0):
-	for item in my_list:
-		if idx < 0:
-			item.remove()
-			print(my_list)
-		else:
-			print(my_list)
+    if 0 <= idx < len(my_list):
+        del my_list[idx]
+        return my_list
+    else:
+        return (my_list)
