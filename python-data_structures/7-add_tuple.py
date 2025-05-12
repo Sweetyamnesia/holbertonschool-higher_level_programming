a77aee8bb9c6443e249323c0d37dfe05f6cbd736
Sweetyamnesia <angela.r@live.fr>
2025-05-12 16:41:30 +0200

    modify for condition and add new condition if, replace print by return tuple a + tuple b

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-	for number in tuple_a and tuple_b:
-		number = tuple_a + tuple_b
-		print(number)
+	for n in range (len(tuple_a)):
+		if len(tuple_a) == 0:
+			return
+		else:
+			return(tuple_a + tuple_b)
