2cb780d36fb1fbe2d84947ed2277c8490c554aaa
Sweetyamnesia <angela.r@live.fr>
2025-05-13 15:49:40 +0200

    first try, but problem with result

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def roman_to_int(roman_string):
+    for n in range(1, 3999):
+        if roman_string != str:
+            return 0
+        elif roman_string is None:
+            return 0
+    return roman_string(n)
