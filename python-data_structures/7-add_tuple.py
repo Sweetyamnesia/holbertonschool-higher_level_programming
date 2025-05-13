ab8e04c844da4e156f72a7f2f10a0fe340611e96
Sweetyamnesia <angela.r@live.fr>
2025-05-13 09:44:06 +0200

    Fix add_tuple to handle tuple sizes and return correct sum of first two elements

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-	for n in range (len(tuple_a)):
-		if len(tuple_a) == 0:
-			return
-		else:
-			return(tuple_a + tuple_b)
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    return (a[0] + b[0], a[1] + b[1])
