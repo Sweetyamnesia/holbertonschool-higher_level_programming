fb97c0add41a99d5e4f70f56c3ae57d40e43cb5a
Sweetyamnesia <angela.r@live.fr>
2025-05-13 09:29:06 +0200

    Fix: Print each row of matrix with space-separated integers using nested loops

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-	for number in matrix:
-		print("{}".format(number))   
+    for row in matrix:
+        for i, element in enumerate(row):
+            if i != len(row) - 1:
+                print("{:d}".format(element), end=" ")
+            else:
+                print("{:d}".format(element))
