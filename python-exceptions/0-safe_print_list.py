456f6dfd4935b5c290b4be6598e3070555d86bf9
Sweetyamnesia <angela.r@live.fr>
2025-05-14 14:51:14 +0200

    delete function map and modify by print

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     try:
-        return list(map(lambda x: x > x, my_list))
-    except x < my_list:
-        print("x inférieur à my_list")
+        print("{:d}".format(x))
+        return True
+    except Exception:
+        return False
