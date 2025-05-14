b7c663983009dafeef56fdd0a835c29ee920ec82
Sweetyamnesia <angela.r@live.fr>
2025-05-14 16:34:07 +0200

    fix : change format(x) to format(element) and add end for line

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for element in my_list:
-            print("{:d}".format(x))
+            print("{:d}".format(element), end="\n")
             return True
     except Exception:
         return False
