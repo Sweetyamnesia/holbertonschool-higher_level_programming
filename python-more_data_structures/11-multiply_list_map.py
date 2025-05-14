4ea255e30f671abd54ad7b0458b94c2c20b6f457
Sweetyamnesia <angela.r@live.fr>
2025-05-14 09:41:58 +0200

    Refactor: replace manual loop with map and lambda for multiplying list elements

--- python-more_data_structures/11-multiply_list_map.py
+++ python-more_data_structures/11-multiply_list_map.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 
 def multiply_list_map(my_list=[], number=0):
-    new_list = {}
-    for number in my_list:
-        new_list = number * len(my_list)
-    return new_list
+    return list(map(lambda x: x * number, my_list))
