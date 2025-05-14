7db748f33d6bc42b8b582c5215c6244a4fcd28ec
Sweetyamnesia <angela.r@live.fr>
2025-05-14 17:23:57 +0200

    add print and format inside exception and delete '\n' inside end

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,5 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(element), end="")
             count += 1
     except Exception:
-        print("\n")
+        print("{}".format(element))
     return count
