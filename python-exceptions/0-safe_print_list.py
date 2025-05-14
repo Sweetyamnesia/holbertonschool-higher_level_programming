8b2ff0e1eeff4daba3e14eb41896c44d48be0082
Sweetyamnesia <angela.r@live.fr>
2025-05-14 10:29:25 +0200

    first try

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,2 +1,6 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    try:
+        return list(map(lambda x: x > x, my_list))
+    except x < my_list:
+        print("x inférieur à my_list")
