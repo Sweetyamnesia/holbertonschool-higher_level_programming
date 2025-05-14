873fbfcc28577b92da59ad5baf908711ae9349fd
Sweetyamnesia <angela.r@live.fr>
2025-05-14 08:46:50 +0200

    Fix: ensure print_matrix_integer prints empty line when called with default argument

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
+    if not matrix or matrix == [[]]:
+        print()
+        return
     for row in matrix:
         for i, element in enumerate(row):
             if i != len(row) - 1:
                 print("{:d}".format(element), end=" ")
             else:
                 print("{:d}".format(element))
-    print()
