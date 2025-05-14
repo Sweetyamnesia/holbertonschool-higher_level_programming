c19104120532bd418b415e654ad1dd07a4bc8d46
Sweetyamnesia <angela.r@live.fr>
2025-05-14 11:10:23 +0200

    first try and add Exception as e to print value

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
+    try:
+        if value == int:
+            return True
+        else:
+            return False
+    except Exception as e:
+        print("{:d}".format(value))
