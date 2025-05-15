ddca8d8a22e0943f4b0229d393468a3185c4927c
Sweetyamnesia <angela.r@live.fr>
2025-05-15 12:10:16 +0200

    Refactor list_division: iterate by index, handle errors per element, add validation messages

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,10 +2,19 @@
 def list_division(my_list_1, my_list_2, list_length):
     result = []
     try:
-        for my_list_1, my_list_2 in zip(my_list_1, my_list_2):
-            result.append(my_list_1 / my_list_2)
-    except ZeroDivisionError:
-        return None
+        for i in range(list_length):
+            element1 = my_list_1[i]
+            element2 = my_list_2[i]
+
+            if element1 != int or float or element2 != int or float:
+                print("wrong type")
+                result = 0
+            elif element2 == 0:
+                print("division by 0")
+            else:
+                element1 / element2
+    except IndexError:
+        print("out of range")
     finally:
         print("{:d}".format(list_length))
-        return result
+    return result
