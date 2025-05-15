a1e715de7546e5f150f97b77c19522188b0819c7
Sweetyamnesia <angela.r@live.fr>
2025-05-15 11:21:20 +0200

    fix: count only valid integers and avoid catching unexpected exceptions

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
-    try:
-        count = 0
-        for i in range(x):
+    for i in range(x):
+        try:
+            count = 0
             element = my_list[i]
             print("{:d}".format(element), end="")
             count += 1
-    except Exception:
-        pass
+        except (ValueError, TypeError):
+            continue
 
     print()
     return count
