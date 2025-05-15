88df3859a66a1f3394b1a7728e3003129d1a9afa
Sweetyamnesia <angela.r@live.fr>
2025-05-15 14:55:36 +0200

    first try: add documentation and write code but test failed : 9 failures

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,3 +1,25 @@
 #!/usr/bin/python3
+"""
+    Adds two numbers after checking their type.
+
+    Numbers must be integers or floats.
+    If they are floats, they are first converted to integers.
+
+    Args:
+    a (int or float): The first operand of the addition.
+    b (int or float, optional): The second operand. Defaults to 98.
+
+    Returns:
+    int: The integer sum of a and b.
+
+    Raises:
+    TypeError: If a or b is neither an int nor a float.
+"""
+
+
 def add_integer(a, b=98):
-    
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
