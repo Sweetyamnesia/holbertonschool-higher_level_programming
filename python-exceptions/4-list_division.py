9f7c33830ee38f7841c75fc13d4d5caaee514a17
Sweetyamnesia <angela.r@live.fr>
2025-05-15 12:25:16 +0200

    Move try block inside loop and replace finally print with pass

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     result = []
-    try:
-        for i in range(list_length):
+    for i in range(list_length):
+        try:
             element1 = my_list_1[i]
             element2 = my_list_2[i]
 
@@ -13,8 +13,8 @@ def list_division(my_list_1, my_list_2, list_length):
                 print("division by 0")
             else:
                 result.append(element1 / element2)
-    except IndexError:
+        except IndexError:
             print("out of range")
-    finally:
-            print("{:d}".format(list_length))
+        finally:
+            pass
     return result
