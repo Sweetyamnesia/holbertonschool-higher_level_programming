40d307d6f011ea304e215a7f1060b207b075b303
Sweetyamnesia <angela.r@live.fr>
2025-05-15 10:52:45 +0200

    Add basic structure for list_division with try/except/finally and division logic

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
+    result = []
     try:
-        for element in my_list_1:
-            print("{:d}".format(element))
-            return True
-    except Exception:
-        return False
+        for my_list_1, my_list_2 in zip(my_list_1, my_list_2):
+            result.append(my_list_1 / my_list_2)
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("{:d}".format(list_length))
+        return result
