f4caf0374da35f53e3d4e660c289438bec5faa54
Sweetyamnesia <angela.r@live.fr>
2025-05-15 10:06:34 +0200

    fix: ensure safe_print_list ends with newline and handles exceptions silently

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -7,5 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(element), end="")
             count += 1
     except Exception:
-        print("{}".format(element))
+        pass
+
+    print()
     return count
