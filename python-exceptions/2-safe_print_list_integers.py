d4e4a3ba23953d845c58909ac49c6e2828678a59
Sweetyamnesia <angela.r@live.fr>
2025-05-15 11:23:08 +0200

    fix: count outside the loop for

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    count = 0
     for i in range(x):
         try:
-            count = 0
             element = my_list[i]
             print("{:d}".format(element), end="")
             count += 1
