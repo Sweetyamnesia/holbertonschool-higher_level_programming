f3ec1a2233a5c9b9560ad142543872a80eeba9ea
Sweetyamnesia <angela.r@live.fr>
2025-05-16 13:21:42 +0200

    add comment inside function

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -5,19 +5,23 @@
     Numbers must be integers or floats.
     If they are floats, they are first converted to integers.
 
-    Args:
-    a (int or float): The first operand of the addition.
-    b (int or float, optional): The second operand. Defaults to 98.
-
-    Returns:
-    int: The integer sum of a and b.
-
     Raises:
     TypeError: If a or b is neither an int nor a float.
 """
 
 
 def add_integer(a, b=98):
+    """
+    Function that prints sum of 2 integers.
+
+    Args:
+    a (int or float): The first operand of the addition.
+    b (int or float, optional): The second operand. Defaults to 98.
+
+    Returns:
+    int: The integer sum of a and b.
+    """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     elif not isinstance(b, (int, float)):
