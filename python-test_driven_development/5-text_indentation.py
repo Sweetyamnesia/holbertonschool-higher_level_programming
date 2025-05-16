68a2f210930d5bcc39692d4bbb4e0e9ff31a60d4
Sweetyamnesia <angela.r@live.fr>
2025-05-16 11:21:32 +0200

    add comment before function

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+Prints a text with 2 new lines after each of theses characters . ? and :
+
+TypeError : text must be a string
+
+"""
+
+
 def text_indentation(text):
     if not isinstance(text, (str)):
         raise TypeError("text must be a string")
