e26b8d5a41c808fb3985ded8c6b85c63acb5a7f2
Sweetyamnesia <angela.r@live.fr>
2025-05-16 13:32:08 +0200

    add comment to the function

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -7,7 +7,18 @@ TypeError: First must be strings.
 TypeError: Last name must be a string
 """
 
+
 def say_my_name(first_name, last_name=""):
+    """
+    Function that prints first name and last name.
+
+    Args:
+    first_name: The first argument.
+    last_name: The second argument
+
+    Returns:
+    string with first and last name
+    """
     if not isinstance(first_name, (str)):
         raise TypeError("first_name must be a string")
     elif not isinstance(last_name, (str)):
