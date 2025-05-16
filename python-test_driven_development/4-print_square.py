9cd5682ceaab7892835f394543546177f09b49a9
Sweetyamnesia <angela.r@live.fr>
2025-05-16 11:06:22 +0200

    first try: 9 failures

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
+"""
+Function that prints a square with character #
+
+Size is the lenght of the square.
+
+TypeError: Size must be an integer.
+
+TypeError: Size must be >= 0
+
+TypeError: size must be an integer
+
+"""
+
+
 def print_square(size):
-	
+    if not isinstance(size, (int)):
+        raise TypeError("size must be an integer")
+    elif size < 0:
+        raise ValueError("size must be >= 0")
+    elif not isinstance(size, (float)) and size < 0:
+        raise TypeError("size must be an integer")
+    for i in range(size):
+        print("#" * size)
