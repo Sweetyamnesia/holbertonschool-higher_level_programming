67218856990496304c9703f7d102c27bb2336a21
Sweetyamnesia <angela.r@live.fr>
2025-05-16 09:51:50 +0200

    first try: 0 passed and 0 failed, then, 0 passed and 2 failed

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
+"""
+Function that prints a string with two parameters : first name and last name.
+
+TypeError: First must be strings.
+
+TypeError: Last name must be a string
+"""
+
 def say_my_name(first_name, last_name=""):
-	
+	if not isinstance(first_name, (str)):
+		raise TypeError("first_name must be a string")
+	elif not isinstance(last_name, (str)):
+		raise TypeError("last_name must be a string")
+	print(f"My name is {first_name} {last_name}")
