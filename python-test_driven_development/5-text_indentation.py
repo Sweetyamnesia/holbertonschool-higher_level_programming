3615f3a161534e9dc8790b9630aa7b641b125b9a
Sweetyamnesia <angela.r@live.fr>
2025-05-17 02:40:33 +0200

    Fix: Correct extra blank lines after punctuation in text_indentation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -21,13 +21,15 @@ def text_indentation(text):
     if not isinstance(text, (str)):
         raise TypeError("text must be a string")
 
-    new_line = False
-    for char in text:
-        if new_line and char == " ":
-            continue
-        new_line = False
-
-        print(char, end="")
-        if char in [".", "?", ":"]:
-            print()
-            new_line = True
+    i = 0
+    while i < len(text):
+        if text[i] in ['.', '?', ':']:
+            print(text[i], end="")
+            print('\n')
+            # skip spaces after punctuation
+            i += 1
+            while i < len(text) and text[i] == ' ':
+                i += 1
+        else:
+            print(text[i], end="")
+            i += 1
