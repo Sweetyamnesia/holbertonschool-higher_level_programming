e0150928e4b70fe284622bc44966b1cedc6a1146
Sweetyamnesia <angela.r@live.fr>
2025-05-18 19:58:11 +0200

    Refactor max_integer tests: corrected expected values and handled edge cases

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -10,13 +10,13 @@ class TestMaxInteger(unittest.TestCase):
       self.assertEqual(max_integer([1, 2, 3, 4]), 4)
 
     def test_max_at_the_beginning(self):
-       self.assertEqual(max_integer([10, 9, 8, 7], 10))
+       self.assertEqual(max_integer([10, 9, 8, 7]), 10)
 
     def test_max_in_the_middle(self):
-       self.assertEqual(max_integer([100, 200, 300, 400, 500]), 300)
+       self.assertEqual(max_integer([100, 200, 300, 250, 50]), 300)
 
     def test_one_negative_number(self):
-       self.assertEqual(max_integer([12, 13, 14, -15]), -15)
+       self.assertEqual(max_integer([12, 13, 14, -15]), 14)
 
     def test_one_element(self):
        self.assertEqual(max_integer([1000]), 1000)
