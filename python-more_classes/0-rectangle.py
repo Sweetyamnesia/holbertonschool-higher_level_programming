7e5d7227a55f95c7891631b3e1f273e5ee9d29e6
Sweetyamnesia <angela.r@live.fr>
2025-05-19 17:57:31 +0200

    create class rectangle

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""
+This module defines a class named Rectangle.
+It is a basic example to illustrate object-oriented programming in Python.
+"""
+
+
+class Rectangle:
+    """
+    Represents an empty rectangle.
+
+    This is a placeholder class used to illustrate the structure
+    of a class in Python.
+    """
+    pass
