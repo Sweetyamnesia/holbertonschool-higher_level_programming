d39331ed4ffb5a0ca68d027e802954c3cbcaa7d4
Sweetyamnesia <angela.r@live.fr>
2025-05-19 18:54:10 +0200

    Rewrite Rectangle class to try to fix test failure

--- python-more_classes/0-rectangle.py
+++ python-more_classes/0-rectangle.py
@@ -8,7 +8,5 @@ It is a basic example to illustrate object-oriented programming in Python.
 class Rectangle:
     """
     Represents an empty rectangle.
-
-    This is a placeholder class used to illustrate the structure
-    of a class in Python.
     """
+    pass
