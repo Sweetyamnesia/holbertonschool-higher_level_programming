c50e39ec97efee4a8349754b3aab803cde85d620
Sweetyamnesia <angela.r@live.fr>
2025-05-19 14:25:59 +0200

    create an empty class square

---
+++ python-classes/0-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+This module defines a class named Square.
+It is a basic example to illustrate object-oriented programming in Python.
+"""
+
+
+class Square:
+    """
+    A class that represents a square.
+
+    Currently, this class does not define any attributes or methods.
+    """
+    pass
+
+
+# Create an instance of the Square class
+s = Square()
+print(s)
