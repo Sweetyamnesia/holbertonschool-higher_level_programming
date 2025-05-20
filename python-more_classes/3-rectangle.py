c5de2c57e283917d740caf514e25eeb8d3304be9
Sweetyamnesia <angela.r@live.fr>
2025-05-20 09:58:26 +0200

    replace the final print with return

--- python-more_classes/3-rectangle.py
+++ python-more_classes/3-rectangle.py
@@ -115,4 +115,5 @@ class Rectangle:
             print()
         else:
             for _ in range(self.height):
-                print("#" * self.height)
+                return "#" * self.height
+
