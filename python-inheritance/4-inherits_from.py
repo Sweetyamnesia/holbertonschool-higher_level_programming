93dba95ca7e59fe3972bdabc1ec84aca8ae32c22
Sweetyamnesia <angela.r@live.fr>
2025-05-26 14:57:17 +0200

    problem : print the bool

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Module that defines the `is_same_class` function
+to verify instance of an object.
+"""
+
+
+def inherits_from(obj, a_class):
+    """
+    Args:
+        obj: Any Python object.
+        a_class: the specified class
+
+    Returns:
+        True : if the object is an instance of a class
+        or if it's inherited from the specified class
+        False: if is not an instance of the class
+    """
+
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
