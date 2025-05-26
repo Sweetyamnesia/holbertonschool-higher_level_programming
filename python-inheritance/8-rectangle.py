f64fd603274b0d1ab283e0f754d28aa4783a9222
Sweetyamnesia <angela.r@live.fr>
2025-05-26 16:05:21 +0200

    first try: missed the type error for height

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,1 +1,32 @@
 #!/usr/bin/python3
+"""
+This module defines a class named BaseGeometry.
+It is a basic example to illustrate object-oriented programming in Python.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents an empty Base Geometry.
+
+    This is a placeholder class used to illustrate the structure
+    of a class in Python.
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        self.name = name
+        self.value = value
+        if not isinstance(value, int):
+            raise TypeError(f'{name} must be an integer')
+        if value <= 0:
+            raise ValueError(f'{name} must be greater than 0')
+
+
+class Rectangle(BaseGeometry):
+    def __init__(self, width, height):
+        self.__width = width
+        self.__height = height
