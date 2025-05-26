1d6fc11b9c380b50aa3f72d9fbebb87186e6aea6
Sweetyamnesia <angela.r@live.fr>
2025-05-26 14:13:42 +0200

    first try: verification of the instance

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Module that defines the `is_same_class` function
+to verify instance of an object.
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    Args:
+        obj: Any Python object.
+        a_class: the specified class
+
+    Returns:
+        True : if the object is exactly an instance of the specified class
+        False: if is not an instance of the specified class
+    """
+    if not isinstance(obj, a_class):
+        return True
+    else:
+        return False
