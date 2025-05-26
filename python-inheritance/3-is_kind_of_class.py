3b42d40ebccae403ef8ee74d2c343bd3f44b15fd
Sweetyamnesia <angela.r@live.fr>
2025-05-26 14:49:02 +0200

    modify type by isinstance()

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -16,7 +16,7 @@ def is_kind_of_class(obj, a_class):
         False: if is not an instance of the class
     """
 
-    if type(obj) is a_class:
+    if isinstance(obj, a_class):
         return True
     else:
         return False
