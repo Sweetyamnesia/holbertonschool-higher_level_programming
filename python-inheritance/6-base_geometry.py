b3f2fdbcb244e5dc45746067706242025218e16c
Sweetyamnesia <angela.r@live.fr>
2025-05-26 15:26:10 +0200

    add raise exception

--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""
+This module defines a class named BaseGeometry.
+It is a basic example to illustrate object-oriented programming in Python.
+"""
+
+
+class BaseGeometry:
+    """
+    Represents an empty Base Geometry.
+
+    This is a placeholder class used to illustrate the structure
+    of a class in Python.
+    """
+    pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
