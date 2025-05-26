511b4d162f20089b2585583bb0091009e7361237
Sweetyamnesia <angela.r@live.fr>
2025-05-26 12:30:06 +0200

    add sorted to print list in ascending order

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""Module that defines the `print_sorted` function
+to write a class that inherits from a list
+"""
+
+
+class MyList(list):
+
+    def print_sorted(self):
+        """
+        Returns the list in ascending sort.
+
+        Args:
+            self.
+
+        Returns:
+            list: A list of int
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
