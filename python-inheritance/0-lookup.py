f502ca86ade33a435be030debf64e77ca6f2d57c
Sweetyamnesia <angela.r@live.fr>
2025-05-26 11:17:37 +0200

    first try : print object attributes

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""
+Function that returns the list of available attributes
+and methods of an object
+
+Return : a list of object
+"""
+
+
+def lookup(obj):
+    print(vars(obj))
