86590c0b7298d838c1e3860a66c4800ccd8a194d
Sweetyamnesia <angela.r@live.fr>
2025-05-27 11:41:04 +0200

    delete self.name and self.value

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -17,8 +17,6 @@ class BaseGeometry:
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        self.name = name
-        self.value = value
         if type(value) is not int:
             raise TypeError(f'{name} must be an integer')
         if value <= 0:
