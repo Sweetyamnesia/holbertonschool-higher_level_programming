bfedc1e518d2f5d475e8dcec5f6de25986545e6e
Sweetyamnesia <angela.r@live.fr>
2025-05-28 10:59:55 +0200

    call area() and perimeter() methods in shape_info to print their results

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -1,1 +1,45 @@
 #!/usr/bin/env python3
+from abc import ABC, abstractmethod
+import math
+
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+
+    @abstractmethod
+    def perimeter(self):
+        pass
+
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        radius = 0
+        area = math.pi * radius ** 2
+        return area
+
+    def perimeter(self):
+        radius = 0
+        perimeter = 2 * math.pi * radius
+        return perimeter
+
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def area(self):
+        return (self.width * self.height)
+
+    def perimeter(self):
+        return (2 * self.width + 2 * self.height)
+
+
+def shape_info(shape):
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
