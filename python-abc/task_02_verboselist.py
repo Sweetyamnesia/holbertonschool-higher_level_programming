83d20a3973cfa6ac52a07e98d12fa991aeb0d915
Sweetyamnesia <angela.r@live.fr>
2025-05-28 12:50:38 +0200

    add super() function for each notifications

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 class VerboseList(list):
     def append(self, item):
+        super().append(item)
         print(f"Added {item} to the list.")
 
     def extend(self, number):
+        super().extend(number)
         print(f"Extended the list with {number} items.")
 
     def remove(self, item):
+        super().remove(item)
         print(f"Remove {item} from the list.")
 
     def pop(self, item):
+        super().pop(item)
         print(f"Popped {item} from the list.")
