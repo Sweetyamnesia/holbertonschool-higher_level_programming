d1dd50e8d87bc859944fc48c75933ac0338ae88e
Sweetyamnesia <angela.r@live.fr>
2025-05-28 13:07:46 +0200

    set up countediterator class, problem with number of items

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+class CountedIterator:
+    def __init__(self, some_iterable):
+        self.iterator = iter(some_iterable)
+        self.count = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        value = next(self.iterator)
+        return value
+
+    def get_count(self):
+        return self.count
