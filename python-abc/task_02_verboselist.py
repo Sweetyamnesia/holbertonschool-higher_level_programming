e588eee6c3f012a652ac1b22911ba79a40ad7b84
Sweetyamnesia <angela.r@live.fr>
2025-05-28 12:43:19 +0200

    add append, extend, remove and pop

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,1 +1,13 @@
 #!/usr/bin/env python3
+class VerboseList(list):
+    def append(self, item):
+        print(f"Added {item} to the list.")
+
+    def extend(self, number):
+        print(f"Extended the list with {number} items.")
+
+    def remove(self, item):
+        print(f"Remove {item} from the list.")
+
+    def pop(self, item):
+        print(f"Popped {item} from the list.")
