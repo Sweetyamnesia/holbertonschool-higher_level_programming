ac819c51a9b044710668b849db30dc130a71d2bb
Sweetyamnesia <angela.r@live.fr>
2025-05-29 17:11:53 +0200

    add comment

--- python-abc/task_05_dragon.py
+++ python-abc/task_05_dragon.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
+
+# Mixin that provides swimming ability
 class SwimMixin:
     def swim(self):
         print("The creature swims!")
 
 
+# Mixin that provides flying ability
 class FlyMixin:
     def fly(self):
         print("The creature flies!")
 
 
+# Dragon class inherits both swimming and flying abilities via mixins
 class Dragon(SwimMixin, FlyMixin):
     def roar(self):
-        print("The dragon roars")
+        print("The dragon roars!")
