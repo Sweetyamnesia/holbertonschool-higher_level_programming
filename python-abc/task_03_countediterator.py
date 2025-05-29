64612defda8320213f912f7e218edc2c9356944b
Sweetyamnesia <angela.r@live.fr>
2025-05-29 16:49:13 +0200

    add comments and incrementation of self.count

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python3
+
+# Custom iterator that keeps track of how many items have been accessed
 class CountedIterator:
     def __init__(self, some_iterable):
+        # Convert the input iterable into an iterator
         self.iterator = iter(some_iterable)
+        # Initialize the counter
         self.count = 0
 
     def __iter__(self):
+        # Returns the iterator object (self)
         return self
 
     def __next__(self):
+        # Increment self.count before returning the next item
+        self.count += 1
         value = next(self.iterator)
         return value
 
     def get_count(self):
+        # Returns the number of items that have been fetched so far
         return self.count
