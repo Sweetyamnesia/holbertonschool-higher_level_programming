6d9f5e5c8712441dd95603f709165c72d53d307f
Sweetyamnesia <angela.r@live.fr>
2025-06-02 14:51:44 +0200

    add attrs inside public methode

---
+++ python-input_output/10-student.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+This module contains a class Student defined by instances attributes.
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        "function that retrieves a dictionary representation of Student"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        return self.__dict__
