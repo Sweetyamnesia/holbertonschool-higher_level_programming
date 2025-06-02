8fb45378c28d18d845be3b8a776bc4ae3a9a0741
Sweetyamnesia <angela.r@live.fr>
2025-06-02 11:46:33 +0200

    problem with output : only one line

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
+"""
+This module contains a function that append a string at the end of a text file
+and returns the number of characters added.
+"""
+
 
 def append_write(filename="", text=""):
-	
+    """Prints number of characters which added at the end of text file."""
+    with open(filename, "w", encoding="utf-8") as file:
+        content = file.write(text)
+    return (content)
