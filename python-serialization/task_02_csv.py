9a23397096ef7a856a138f7daf8d6e5cb90d9d2e
Sweetyamnesia <angela.r@live.fr>
2025-06-02 20:09:48 +0200

    feat: convert CSV to JSON using DictReader and json.dump

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -8,7 +8,9 @@ def convert_csv_to_json(filename):
     try:
         with open(filename, "r") as csv_file:
             data = list(csv.DictReader(csv_file))
-        with open(data, "w") as jsonfile:
+
+        jsonfile = filename.replace(".csv", ".json")
+        with open(jsonfile, "w") as jsonfile:
             json.dump(data, jsonfile)
             return True
     except Exception:
