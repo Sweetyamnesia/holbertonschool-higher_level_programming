678c46a23bc89b3403f10b064580667d664b29e5
Sweetyamnesia <angela.r@live.fr>
2025-06-02 14:19:42 +0200

    Refactor add_item script: remove manual file handling, use functions, fix list structure

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -5,16 +5,15 @@ and save them to a file.
 """
 
 
+import sys
 import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-
-def load_from_json_file(filename):
-    """Function which create an object"""
-    with open(filename, "r") as file:
-        return json.load(file)
-
-
-def save_to_json_file(my_obj, filename):
-    """Function which returns Object to a text file"""
-    with open(filename, "w") as file:
-        json.dump(my_obj, file)
+args = sys.argv
+filename = "add_item.json"
+with open(filename, 'a', encoding="utf-8") as file:
+    my_list = []
+    my_list.append(args[1:])
+    save_to_json_file(my_list, filename)
+    load_from_json_file(filename)
