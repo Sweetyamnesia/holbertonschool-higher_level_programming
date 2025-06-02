9cdd5e42852fae2c18623d1041010b4d9ac7ba69
Sweetyamnesia <angela.r@live.fr>
2025-06-02 11:59:17 +0200

    return str object

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+This module contains a function that returns JSON
+representation of an object (string).
+"""
+
+
+def to_json_string(my_obj):
+    """Function which returns the JSON string"""
+    return str(my_obj)
