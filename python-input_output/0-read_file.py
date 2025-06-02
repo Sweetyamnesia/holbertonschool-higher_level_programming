4a0eeee653034db6ae95f5cbde1c5ca8899d137d
Sweetyamnesia <angela.r@live.fr>
2025-06-02 10:46:24 +0200

    add enconding utf-8

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ the contents of a text file.
 
 def read_file(filename=""):
     """Reads a text file and prints its contents."""
-    with open(filename, "r") as file:
+    with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
     print(content)
