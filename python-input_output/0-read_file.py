b339f80d4772cb1c2cfa3b9bf4a1728348ffac40
Sweetyamnesia <angela.r@live.fr>
2025-06-02 11:39:41 +0200

    add end=

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,4 +9,4 @@ def read_file(filename=""):
     """Reads a text file and prints its contents."""
     with open(filename, "r", encoding="utf-8") as file:
         content = file.read()
-    print(content)
+    print(content, end="")
