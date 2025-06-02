8c72979d169f6223032d3d064f508ecd0eee5875
Sweetyamnesia <angela.r@live.fr>
2025-06-02 19:12:40 +0200

    create class CustomObject and use pickle module

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -1,1 +1,33 @@
 #!/usr/bin/env python3
+
+"""
+This module is used to learn how to serialize and deserialize
+custom Python objects using the pickle module.
+"""
+
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = is_student
+
+    def serialize(self, filename):
+        """
+         Serializes the current object to a binary file using pickle.
+        """
+        with open(filename, "wb") as file:
+            pickle.dump(filename, file)
+        pass
+
+
+@classmethod
+def deserialize(cls, filename):
+    """
+    Deserializes an object from a binary file using pickle.
+    """
+    with open(filename, "rb") as file:
+        return pickle.load(file)
+    pass
