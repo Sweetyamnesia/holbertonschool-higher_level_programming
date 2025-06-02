2be88b89832f0ca234796fe24c90553eafb9baee
Sweetyamnesia <angela.r@live.fr>
2025-06-02 14:09:45 +0200

    delete double function load and replace with save_to_json_file function

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -14,7 +14,7 @@ def load_from_json_file(filename):
         return json.load(file)
 
 
-def load_from_json_file(filename):
-    """Function which create an object"""
-    with open(filename, "r") as file:
-        return json.load(file)
+def save_to_json_file(my_obj, filename):
+    """Function which returns Object to a text file"""
+    with open(filename, "w") as file:
+        json.dump(my_obj, file)
