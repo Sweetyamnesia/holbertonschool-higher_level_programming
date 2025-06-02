5eaef10b17ef7deb3e0ce071522610641d3288c7
Sweetyamnesia <angela.r@live.fr>
2025-06-02 20:00:53 +0200

    use DictReader class and csv and json modules

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -1,1 +1,15 @@
 #!/usr/bin/env python3
+
+import csv
+import json
+
+
+def convert_csv_to_json(filename):
+    try:
+        with open(filename, "r") as csv_file:
+            data = list(csv.DictReader(csv_file))
+        with open(data, "w") as jsonfile:
+            json.dump(data, jsonfile)
+            return True
+    except Exception:
+        return False
