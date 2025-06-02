cc79c2b568362fcebb1e7dfc3cb1bfa262613337
Sweetyamnesia <angela.r@live.fr>
2025-06-02 20:46:44 +0200

    save file

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+
+import xml.etree.ElementTree as ET
+
+
+def serialize_to_xml(dictionary, filename):
+	data = ET.Element('data')
+	for child in root:
+		print(dictionary, filename)
+
+
+def deserialize_from_xml(filename):
+	tree = ET.parse("dict.xml")
+	filename = tree.getroot()
