770dc740b18e327985f524f60d2660fef1546100
Sweetyamnesia <angela.r@live.fr>
2025-06-02 18:48:57 +0200

    add 2 functions

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+"""
+This module adds the functionality to serialize a Python dictionary
+to a JSON file and deserialize the JSON file to recreate the Python Dictionary.
+"""
+
+import json
+
+
+def serialize_and_save_to_file(data, filename):
+    """
+    Function with Python Dictionary with data and
+    the filename of the ouput JSON file
+    """
+    with open(filename, "w") as file:
+        json.dump(data, file)
+    pass
+
+
+def load_and_deserialize(filename):
+    """
+    Function with the filename of the input JSON file.
+    """
+    with open(filename, "r") as file:
+        return json.load(file)
+    pass
