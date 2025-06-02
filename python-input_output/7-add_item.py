24152dfa2190c3abcf0995d2c0936466f062e07a
Sweetyamnesia <angela.r@live.fr>
2025-06-02 14:22:03 +0200

    modify args and my_list

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -10,10 +10,8 @@ import json
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-args = sys.argv
+args = sys.argv[1:]
 filename = "add_item.json"
 with open(filename, 'a', encoding="utf-8") as file:
-    my_list = []
-    my_list.append(args[1:])
+    my_list = load_from_json_file(filename)
     save_to_json_file(my_list, filename)
-    load_from_json_file(filename)
