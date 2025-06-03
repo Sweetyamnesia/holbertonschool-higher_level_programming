4cd78f86271234445ebd34557f0afd763f0be7c4
Sweetyamnesia <angela.r@live.fr>
2025-06-03 14:10:17 +0200

    add function which convert dictionary to key/value pairs in XML

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -4,11 +4,23 @@ import xml.etree.ElementTree as ET
 
 
 def serialize_to_xml(dictionary, filename):
-	data = ET.Element('data')
-	for child in root:
-		print(dictionary, filename)
+    """
+    Create the root element
+    """
+    root = ET.Element('data')
+
+    element = element(filename)
+    for key, value in dictionary.items():
+        "Create an element"
+        child = element(key)
+        child.text = str(value)
+        element.append(child)
+    return element
 
 
 def deserialize_from_xml(filename):
-	tree = ET.parse("dict.xml")
-	filename = tree.getroot()
+    tree = ET.parse(filename)
+    root = tree.getroot()
+
+    for child in root:
+        print(child.tag, child.attrib)
