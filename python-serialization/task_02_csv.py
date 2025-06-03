c21fdbecaab7aea3101ab0f05d4d1a6d294d759f
Sweetyamnesia <angela.r@live.fr>
2025-06-03 12:57:27 +0200

    add comment

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -5,13 +5,25 @@ import json
 
 
 def convert_csv_to_json(filename):
+    """
+    Convert a CSV file to a JSON file.
+
+    Args:
+        filename (str): The path to the CSV file to convert.
+
+    Returns:
+        bool: True if conversion succeeds, False otherwise.
+    """
+    
     try:
         with open(filename, "r", encoding="utf-8") as csv_file:
             data = list(csv.DictReader(csv_file))
 
+        "Create the JSON filename by replacing the .csv extension with .json"
         json_filename = filename.replace(".csv", ".json")
         with open(json_filename, "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, indent=4)
-            return True
+        return True
     except Exception:
+        print("file not found")
         return False
