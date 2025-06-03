13b30034a7da9b5f86340c44769724acef1750cd
Sweetyamnesia <angela.r@live.fr>
2025-06-03 10:42:51 +0200

    delete import json

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -5,9 +5,6 @@ description with simple data structure for JSON serialization of an object.
 """
 
 
-import json
-
-
 def class_to_json(obj):
 	"""
 	Function that returns dictionary description of an object
