7c729625ec83837fd16a1caf8e4cef1c6f4e1255
Sweetyamnesia <angela.r@live.fr>
2025-06-03 09:47:41 +0200

    Fix: use load/save functions and preserve existing items in add_item.json

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -4,12 +4,19 @@ This module contains a script which adds all arguments to a Python list
 and save them to a file.
 """
 
-
 import sys
 import json
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 args = sys.argv[1:]
 filename = "add_item.json"
-with open(filename, 'w', encoding="utf-8") as file:
-    json.dump(args, file)
+if filename:
+    items = load_from_json_file(filename)
+else:
+    items = []
+
+items.extend(args)
+
+save_to_json_file(items, filename)
