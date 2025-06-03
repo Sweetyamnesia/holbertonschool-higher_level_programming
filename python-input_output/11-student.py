471eea6c0b2e8f718e4027bd26f7f32ce9adac52
Sweetyamnesia <angela.r@live.fr>
2025-06-03 11:57:07 +0200

    add function reload

---
+++ python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a class Student defined by instances attributes.
+"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        "Initialize instance of the class Student"
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        "function that retrieves a dictionary representation of Student"
+        if attrs is None:
+            return self.__dict__
+        if isinstance(attrs, list):
+            return {key: value for key,
+                    value in self.__dict__.items() if key in attrs}
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        "function that hat replaces all attributes of the Student instance"
+        return {key: value for key,
+                value in self.__dict__.items() if key in json}
