f2ab857d21df00129c5751b6b3e56b3605bc584d
Sweetyamnesia <angela.r@live.fr>
2025-06-03 10:44:12 +0200

    pycodestyle

--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -6,8 +6,7 @@ description with simple data structure for JSON serialization of an object.
 
 
 def class_to_json(obj):
-	"""
-	Function that returns dictionary description of an object
-	"""
-	return obj.__dict__
-	
+    """
+    Function that returns dictionary description of an object
+    """
+    return obj.__dict__
