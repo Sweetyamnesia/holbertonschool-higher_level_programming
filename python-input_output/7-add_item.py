8b7e38998113986a84355656b0ce255ecf574fbb
Sweetyamnesia <angela.r@live.fr>
2025-06-03 09:34:51 +0200

    delete functions and use json.dump

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -9,20 +9,7 @@ import sys
 import json
 
 
-def save_to_json_file(my_obj, filename):
-    """Function which returns Object to a text file"""
-    with open(filename, "w") as file:
-        json.dump(my_obj, file)
-
-
-def load_from_json_file(filename):
-    """Function which create an object"""
-    with open(filename, "r") as file:
-        return json.load(file)
-
-
 args = sys.argv[1:]
 filename = "add_item.json"
 with open(filename, 'w', encoding="utf-8") as file:
-    my_list = load_from_json_file(filename)
-    save_to_json_file(my_list, filename)
+    json.dump(args, file)
