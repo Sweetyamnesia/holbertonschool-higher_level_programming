60477b7ba509a805cd2d1c07dcb2c1f0da8bfa0f
Sweetyamnesia <angela.r@live.fr>
2025-06-03 11:15:37 +0200

    fix: update to_json method to support filtering attrs list and ensure dictionary return

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -6,11 +6,16 @@ This module contains a class Student defined by instances attributes.
 
 class Student:
     def __init__(self, first_name, last_name, age):
-        "function that retrieves a dictionary representation of Student"
+        "Initialize instance of the class Student"
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
+        "function that retrieves a dictionary representation of Student"
         if attrs is None:
             return self.__dict__
+        if isinstance(attrs, list):
+            return {key: value for key,
+                    value in self.__dict__.items() if key in attrs}
+        return self.__dict__
