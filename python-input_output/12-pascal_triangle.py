1f17cf2c43d9df1b74261a0e249a6cda79653a39
Sweetyamnesia <angela.r@live.fr>
2025-06-04 10:45:18 +0200

    TypeError: 'int' object is not iterable

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module to print Pascal's Triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Function that returns a list of list of integers.
+    """
+    for element in n:
+        print("{:d}".format(element))
