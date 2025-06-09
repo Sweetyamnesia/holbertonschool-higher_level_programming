3010724ff375ea85075c0c0bd6e2ef1eaa253dcf
Sweetyamnesia <angela.r@live.fr>
2025-06-09 13:42:25 +0200

    feat: write posts data to CSV file in fetch_and_save_posts()

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -12,7 +12,15 @@ def fetch_and_print_posts():
 
 
 def fetch_and_save_posts():
-    fields = ["id", "title", "body"]
-    with open("posts.csv", "w", encoding="utf-8") as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=fields)
-        writer.writeheader()
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    if response.status_code == 200:
+        data = response.json()
+        with open("posts.csv", "w", encoding="utf-8", newline="") as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=["id", "title", "body"])
+            writer.writeheader()
+            for post in data:
+                writer.writerow({
+                    "id": post["id"],
+                    "title": post["title"],
+                    "body": post["body"]
+                })
