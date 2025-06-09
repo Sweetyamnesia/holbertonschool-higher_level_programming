eb9b374f0641975e6b4db441cabe3e4467587240
Sweetyamnesia <angela.r@live.fr>
2025-06-09 13:46:29 +0200

    pycodestyle

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -3,8 +3,10 @@ import requests
 import json
 import csv
 
+
 def fetch_and_print_posts():
-    response = requests.get("https://jsonplaceholder.typicode.com/posts", data={"key": "value"})
+    response = requests.get("https://jsonplaceholder.typicode.com/posts",
+                            data={"key": "value"})
     if response.status_code == 200:
         data = response.json()
         for post in data:
@@ -16,7 +18,7 @@ def fetch_and_save_posts():
     if response.status_code == 200:
         data = response.json()
         with open("posts.csv", "w", encoding="utf-8", newline="") as csvfile:
-            writer = csv.DictWriter(csvfile, fieldnames=["id", "title", "body"])
+            writer = csv.DictWriter(csvfile)
             writer.writeheader()
             for post in data:
                 writer.writerow({
