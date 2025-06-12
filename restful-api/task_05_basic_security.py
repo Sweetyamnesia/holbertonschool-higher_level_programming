22093cdf1aeb6876abe82594e36e495965b678a6
Sweetyamnesia <angela.r@live.fr>
2025-06-12 16:39:49 +0200

    pycodestyle

--- restful-api/task_05_basic_security.py
+++ restful-api/task_05_basic_security.py
@@ -1,15 +1,36 @@
 #!/usr/bin/python3
 from flask import Flask, jsonify, request
+from flask_jwt_extended.exceptions import (
+    NoAuthorizationError,
+    InvalidHeaderError,
+)
 from flask_httpauth import HTTPBasicAuth
 from flask_jwt_extended import JWTManager, create_access_token
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 
+# Initialize the Flask application
 app = Flask(__name__)
 auth = HTTPBasicAuth()
 
 app.config["JWT_SECRET_KEY"] = "super-secret"
 jwt = JWTManager(app)
+
+
+@jwt.unauthorized_loader
+def unauthorized_callback(callback):
+    return jsonify({"error": "Missing or invalid JWT token"}), 401
+
+
+@jwt.invalid_token_loader
+def invalid_token_callback(callback):
+    return jsonify({"error": "Invalid token"}), 401
+
+
+@jwt.expired_token_loader
+def expired_token_callback(callback, payload):
+    return jsonify({"error": "Token has expired"}), 401
+
 
 users = {
     "John": {
@@ -55,15 +76,25 @@ def basic_protected():
 
 @app.route("/login", methods=["POST"])
 def login():
-    username = request.get_json("username")
-    password = request.get_json("password")
+    data = request.get_json()
+    if not data or "username" not in data or "password" not in data:
+        return jsonify({"error": "Missing username or password"}), 400
+
+    username = data.get("username")
+    password = data.get("password")
     user = users.get(username)
+
     if not user or not check_password_hash(user["password"], password):
         return jsonify({"error": "Bad username or password"}), 401
 
-    access_token = create_access_token(identity={"username": username,
-                                                 "role": user["role"]})
-    return jsonify(access_token=access_token)
+    access_token = create_access_token(
+                                       identity={
+                                                 "username": username,
+                                                 "role": user["role"],
+                                                }
+                                        )
+
+    return jsonify(access_token=access_token), 200
 
 
 @app.route("/jwt-protected")
@@ -80,7 +111,6 @@ def admin_only():
     if identity["role"] != "admin":
         return jsonify({"error": "Access forbidden: Admins only"}), 403
     return jsonify({"message": "Admin Access: Granted"}), 200
-
 
 
 if __name__ == '__main__':
