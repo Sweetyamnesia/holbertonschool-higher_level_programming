6d05046b4512070dc073fabbd804107bc1cd1f5e
Sweetyamnesia <angela.r@live.fr>
2025-06-12 12:08:26 +0200

    fix status and user

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -8,31 +8,36 @@ app = Flask(__name__)
 users = {}
 
 """Root route that returns a welcome message"""
+
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
 
-"""Route to return a list of usernames"""
+
+# Route to return a list of usernames
 @app.route("/data")
 def data():
     return jsonify(list(users.keys()))
 
-"""Route to return the API status"""
+
+# Route to return the API status
 @app.route("/status")
 def status():
-    return jsonify({"status": "OK"})
+    return "OK"
 
-"""Route to get user details by username"""
+
+# Route to get user details by username
 @app.route("/users/<username>")
 def get_user(username):
-    if username in users:
-        user_data = users[username].copy()
-        user_data["username"] = username
-        return jsonify(user_data)
+    user = users.get(username)
+    if user:
+        return jsonify({"username": username, **user})
     else:
         return jsonify({"error": "User not found"}), 404
 
-"""Route to add a new user via POST request"""
+
+# Route to add a new user via POST request
 @app.route("/add_user", methods=["POST"])
 def add_user():
     data = request.get_json()
@@ -61,6 +66,7 @@ def add_user():
         "user": users[username]
     }), 201
 
-"""Run the Flask application if this file is executed directly"""
+
+# Run the Flask application if this file is executed directly
 if __name__ == "__main__":
     app.run(debug=True)
