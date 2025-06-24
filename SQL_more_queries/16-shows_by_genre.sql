eb0dd235b813105d909148dc2f656d72deab2a98
Sweetyamnesia <angela.r@live.fr>
2025-06-24 18:50:07 +0200

    Initial attempt at joining shows and genres; needs adjustment to match expected output

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,2 +1,5 @@
 -- script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows.
-
+SELECT tv_shows.title AS title, tv_genres.name AS name FROM tv_shows, tv_genres
+LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.show_id
+WHERE tv_show_genres.genre_id IS NULL
+ORDER BY tv_shows.title ASC;
