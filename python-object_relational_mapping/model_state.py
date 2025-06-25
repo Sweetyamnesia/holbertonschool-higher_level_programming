a887239751ffdf862fbf06f028fd045df4764864
Sweetyamnesia <angela.r@live.fr>
2025-06-25 17:00:07 +0200

    implementation of the model

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""
+This file contains the class definition of a State and an instance Base.
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(128))
+
+"""
+Access the mapped Table
+"""
+State.__tablename__
+
+"""
+Access the Mapper
+"""
+State.__mapper__
