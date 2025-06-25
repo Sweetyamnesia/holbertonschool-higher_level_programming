ae785cc6522030debc38dcfcce56b6c983891f97
Sweetyamnesia <angela.r@live.fr>
2025-06-25 12:32:31 +0200

    feat: list states starting with 'N' from MySQL database

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -21,9 +21,9 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
-        print(f"{row[0]}: {row[1]}")
+        print(row)
     cur.close()
     db.close()
