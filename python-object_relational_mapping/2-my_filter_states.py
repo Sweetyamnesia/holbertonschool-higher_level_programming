d744dc66e2140383ada4ec3e26d0dd2ee4a7fa73
Sweetyamnesia <angela.r@live.fr>
2025-06-25 12:52:15 +0200

    add an argument for the state

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_0_usa.
+It takes 4 arguments: mysql username, mysql password,
+database name and state name searched.
+Results are sorted by states.id in ascending order.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connects to the MySQL database and retrieves all states sorted by id.
+    Prints each state as a tuple.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    state = sys.argv[4]
+    cur.execute("SELECT * FROM states WHERE name = 'Arizona' ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
