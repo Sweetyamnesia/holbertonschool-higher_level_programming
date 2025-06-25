94d71b5eb6b9b5782b92735a7c9d94199b1f8f8b
Sweetyamnesia <angela.r@live.fr>
2025-06-25 14:33:12 +0200

    Add script to have only cities from Texas

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_4_usa.
+It takes 3 arguments: mysql username, mysql password
+database name and state name.
+Results are sorted by cities.id in ascending order.
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    """
+    Connects to the MySQL database and retrieves all cities sorted by id.
+    Prints each state as a tuple.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    cur.execute("SELECT name FROM cities WHERE state_id=3 ORDER BY id ASC")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
