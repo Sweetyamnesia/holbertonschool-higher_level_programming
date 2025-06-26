c708d7c09740100a1f6b72e36fc5c808fd5abc51
Sweetyamnesia <angela.r@live.fr>
2025-06-26 16:03:30 +0200

    delete the \n

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -25,4 +25,4 @@ first_state = session.query(State).order_by(State.id).first()
 if first_state:
     print(f"{first_state.id}: {first_state.name}")
 else:
-    print("Nothing", end="\n")
+    print("Nothing")
