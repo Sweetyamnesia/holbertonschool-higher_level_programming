ba2e2d979829361d015f3e20b7699a61037b0058
Sweetyamnesia <angela.r@live.fr>
2025-06-26 10:35:32 +0200

    add script to list all states from the database

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,33 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_6_usa.
+It takes 3 arguments: MySQL username, MySQL password, and database name
+Results are sorted by states.id in ascending order.
+"""
+import MySQLdb
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    """
+    Connects to the MySQL database and retrieves all cities sorted by id.
+    Prints each state as a tuple.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    query = "SELECT * FROM states ORDER BY states.id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    print(rows)
+    cur.close()
+    db.close()
