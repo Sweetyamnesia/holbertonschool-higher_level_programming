6dc92b3a527464dd89fb271a7c7200b6c5d37cb2
Sweetyamnesia <angela.r@live.fr>
2025-06-26 10:58:08 +0200

    Try to extract the first state

--- python-object_relational_mapping/8-model_state_fetch_first.py
+++ python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_6_usa.
+It takes 3 arguments: MySQL username, MySQL password, and database name
+Results are sorted by states.id in ascending order.
+"""
+import MySQLdb
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+
+Base = declarative_base()
+
+if __name__ == "__main__":
+    """
+    Connects to the MySQL database and retrieves all cities sorted by id.
+    Prints each state as a tuple.
+    """
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cur = db.cursor()
+    query = "SELECT states.id, states.name FROM states WHERE states.id = 1 ORDER BY states.id ASC"
+    cur.execute(query)
+    rows = cur.fetchall()
+    for row in rows:
+        print(f"{row[0]}: {row[1]}")
+    cur.close()
+    db.close()
