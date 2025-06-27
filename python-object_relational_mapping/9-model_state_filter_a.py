587b344ba86b4f646b1a0c927cc0d0baa0872d91
Sweetyamnesia <angela.r@live.fr>
2025-06-27 09:50:21 +0200

    print all state which contains 'a'

--- python-object_relational_mapping/9-model_state_filter_a.py
+++ python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_6_usa.
+It takes 3 arguments: MySQL username, MySQL password, and database name
+Results are sorted by states.id in ascending order.
+"""
+
+from sys import argv
+from sqlalchemy import create_engine
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = db.create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
+
+    """Bind the engine to a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_a = session.query(State).filter
+    (State.name.like('%a%')).order_by(State.id)
+
+    for a in state_a:
+        print(f"{a.id}: {a.name}")
