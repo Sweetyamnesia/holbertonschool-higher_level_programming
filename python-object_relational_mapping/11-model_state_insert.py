be40518fb203586b500ed57c23f698d82080ab76
Sweetyamnesia <angela.r@live.fr>
2025-06-29 09:20:58 +0200

    feat: initial implementation of script to insert new State with SQLAlchemy

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""
+This script lists all states from the database hbtn_0e_6_usa.
+It takes 3 arguments: MySQL username, MySQL password and database name
+Results are sorted by states.id.
+"""
+
+from sqlalchemy import insert
+from sys import argv
+from sqlalchemy import create_engine
+import sqlalchemy as db
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = db.create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost:3306/{argv[3]}',
+        pool_pre_ping=True
+    )
+
+    """Bind the engine to a session"""
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Louisiana")
+    session.add(new_state)
+    session.commit()
+    print(new_state.id)
