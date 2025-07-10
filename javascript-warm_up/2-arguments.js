c9f5ba75402976c9a6f8f5d56279ce10dbca988c
Sweetyamnesia <angela.r@live.fr>
2025-07-10 18:12:59 +0200

    modification de la variable

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,8 +1,7 @@
-#!/usr/bin/node
-const { args } = require('node:process');
+#!/usr/bin/env node
+const args = process.argv.slice(2);
 
-
-if (args === 0) {
+if (args.length === 0) {
   console.log("No argument");
 } else if (args === 1) {
   console.log("Argument found");
