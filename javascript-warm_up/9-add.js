d50bb7d9e32198811493a8659cdedc60b75c6439
Sweetyamnesia <angela.r@live.fr>
2025-07-10 15:08:29 +0200

    test sans les arguments

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
-const args = process.argv;
-function add(a, b){
+function add(a, b) {
 	return a + b;
 }
-const num1 = parseInt(args[2])
-const num2 = parseInt(args[3])
 
-console.log(num1 + n);
+console.log(add(1, 7));
