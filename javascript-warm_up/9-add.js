5ae5bafd84c4af9a07c83eae2b457bd28977e7bd
Sweetyamnesia <angela.r@live.fr>
2025-07-10 15:10:46 +0200

    retour version précedente

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-function add(a, b) {
+const args = process.argv;
+function add(a, b){
 	return a + b;
 }
+const num1 = parseInt(args[2])
+const num2 = parseInt(args[3])
 
-console.log(add(1, 7));
+console.log(num1 + n);
