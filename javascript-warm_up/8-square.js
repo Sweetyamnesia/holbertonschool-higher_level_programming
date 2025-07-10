8bb90102ff846e7e5769de9383b1972943a21d24
Sweetyamnesia <angela.r@live.fr>
2025-07-10 18:49:41 +0200

    déplacement des deux variables dans la boucle while

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -2,13 +2,13 @@
 const args = process.argv;
 const size = parseInt(args[2]);
 let i = 0;
-let j = 0;
-let line = "";
 
 if (isNaN(size)) {
   console.log("Missing size");
 } else {
   while (i < size) {
+	let line = "";
+	let j = 0;
     while (j < size) {
 		line += "X";
 		j++;
