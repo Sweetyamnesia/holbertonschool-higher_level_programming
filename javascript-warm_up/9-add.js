d24477a30ce0d75b9b9ee89b7a39f012f666e3fe
Sweetyamnesia <angela.r@live.fr>
2025-07-10 15:13:10 +0200

    ajout des ;

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -3,7 +3,8 @@ const args = process.argv;
 function add(a, b){
 	return a + b;
 }
-const num1 = parseInt(args[2])
-const num2 = parseInt(args[3])
+
+const num1 = parseInt(args[2]);
+const num2 = parseInt(args[3]);
 
 console.log(num1 + n);
