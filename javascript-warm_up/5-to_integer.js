c449fefd6e7c69df11de5f1879e1acd71f32e8ce
Sweetyamnesia <angela.r@live.fr>
2025-07-10 18:33:39 +0200

    suppression du guillemet

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const firstArg = args[2];
 const num = parseInt(firstArg, 10);
 
 if (!isNaN(num)){
-	console.log(`My number:" ${num}`);
+	console.log(`My number: ${num}`);
 } else {
 	console.log("Not a number");
 }
