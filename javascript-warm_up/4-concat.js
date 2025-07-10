fa77e451d9ba563f80608f3b3597f481e2259263
Sweetyamnesia <angela.r@live.fr>
2025-07-10 18:29:09 +0200

    suppression de la gestion d'erreur

--- javascript-warm_up/4-concat.js
+++ javascript-warm_up/4-concat.js
@@ -1,8 +1,4 @@
-#!/usr/bin/node
+#!/usr/bin/env node
 const args = process.argv.slice(2);
 
-if (args.length < 2) {
-  console.log("Please provide two arguments");
-} else {
-  console.log(`${args[0]} is ${args[1]}`);
-}
+console.log(`${args[0]} is ${args[1]}`);
