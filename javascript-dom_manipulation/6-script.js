dd409fa6b454930292b3417d9875b993a4e952fa
Sweetyamnesia <angela.r@live.fr>
2025-07-14 09:49:31 +0200

    add name of the star wa&rs character

--- javascript-dom_manipulation/6-script.js
+++ javascript-dom_manipulation/6-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+fetch("https://swapi-api.hbtn.io/api/people/5/?format=json")
+  .then(response => response.json())
+  .then(data => {
+    const name = data.name;
+    document.getElementById("character").textContent = name;
+  })
+  .catch(error => console.error("Error:", error));
