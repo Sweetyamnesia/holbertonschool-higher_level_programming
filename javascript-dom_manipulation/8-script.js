5376dcab16e65027cf645f435f4e73b92e660311
Sweetyamnesia <angela.r@live.fr>
2025-07-14 10:02:38 +0200

    add script to say hello

--- javascript-dom_manipulation/8-script.js
+++ javascript-dom_manipulation/8-script.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+fetch("https://hellosalut.stefanbohacek.dev/?lang=fr")
+  .then(response => response.json())
+  .then(data => {
+    const value = document.getElementById("hello");
+	value.textContent = data.hello
+  })
+  .catch(error => console.error("Error:", error));
