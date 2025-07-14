7ed45aaf73a81b6eb43192f37dc04683757f1f10
Sweetyamnesia <angela.r@live.fr>
2025-07-14 17:09:10 +0200

    add flask and create route

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/items')
+def items():
+    return render_template('items.html')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
