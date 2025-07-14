213b2917b4a0f5ea8caac2d2aa7804e996a197c8
Sweetyamnesia <angela.r@live.fr>
2025-07-14 12:07:17 +0200

    WIP: partial implementation of task 0 (generate_invitations, and add main and template

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+
+def generate_invitations(template, attendees):
+	if not isinstance(template, str):
+		print("Template must be a string")
+	if not (isinstance(attendees, list) and all(isinstance(a, dict) for a in attendees)):
+		print("Attendees must be a a list of dictionaries")
+	if not template:
+		print("Template is empty, no output files generated.")
+	if not attendees:
+		print("No data provided, no output files generated.")
+	
