6bdcd91e22ca3bf3c25ebe60795ca45e5462b2bd
Sweetyamnesia <angela.r@live.fr>
2025-07-14 09:25:01 +0200

    add ocnditions for class elements

--- javascript-dom_manipulation/3-script.js
+++ javascript-dom_manipulation/3-script.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+const header = document.getElementById("toggle_header");
+header.addEventListener("click", function() {
+	const red = document.getElementById("red")
+	if (document.getElementById("red")) {
+		document.getElementById("red") = "green"
+	} else {
+		document.getElementById("red") = "red"
+	}
+});
