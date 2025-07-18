c5ce3d6a80f9e29ce084d844a3c4c0e7b3e24630
Sweetyamnesia <angela.r@live.fr>
2025-07-18 15:20:38 +0200

    change function def

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -20,11 +20,13 @@ def contact():
 @app.route('/items')
 def items():
     try:
-        with open('items.json', 'r') as file:
-            items = json.load(file)
-        return render_template('items.html', items=items.get("items", []))
-    except FileNotFoundError:
-        return render_template('items.html', items=[], error="Items.json file not found.")
+        with open('items.json', 'r') as f:
+            data = json.load(f)
+            items_list = data.get("items", [])
+    except Exception as e:
+        print(f"{e}")
+        items_list = []
+    return render_template('items.html', items=items_list)
 
 
 
