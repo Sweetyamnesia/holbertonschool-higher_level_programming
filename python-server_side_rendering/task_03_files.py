46da773081cd7076ab366306931ab460d681e548
Sweetyamnesia <angela.r@live.fr>
2025-07-18 16:37:03 +0200

    add run

--- python-server_side_rendering/task_03_files.py
+++ python-server_side_rendering/task_03_files.py
@@ -62,3 +62,6 @@ def products():
         return render_template("product_display.html", products=filtered)
 
     return render_template("product_display.html", products=products)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
